ddda834ebd2d641f4c03da1360812bc55de60f4c
max <mapaza@mowa.com.pe>
2025-05-03 15:53:14 -0500

    Primer commit tracker

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, send_file
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/tracker.png')
+def tracker():
+    ip = request.remote_addr
+    ua = request.headers.get('User-Agent')
+    now = datetime.datetime.now().isoformat()
+
+    # Guardar log
+    with open("logs.txt", "a") as f:
+        f.write(f"{now} - IP: {ip} - UA: {ua}\n")
+
+    # Devolver la imagen
+    return send_file("tracker.png", mimetype="image/png")
