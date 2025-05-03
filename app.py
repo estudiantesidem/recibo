b65e992f8ba4c8af78b9ed5ee57952d620b1b270
max <mapaza@mowa.com.pe>
2025-05-03 16:39:02 -0500

    Subida imagen recibo.jpeg y actualizado endpoint

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/recibo.jpeg')  # ✅ Correcto
 def tracker():
-    ip = request.remote_addr
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     ua = request.headers.get('User-Agent')
     now = datetime.datetime.now().isoformat()
     print(f"{now} - IP: {ip} - UA: {ua}")
